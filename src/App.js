2eec0dd55bc8fb57e7546f5c7de8d9fd9cac4219
{eshwargirigowda} <{eshwar.tg.girigowda@gmail.com}>
2025-04-16 01:31:09 +0530

    Pages and Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Sign-In" element={<SignIn />} />
+          <Route path="/Sign-Up" element={<SignUp />} />
+          <Route path="/ForgotPassword" element={<ForgotPassword />} />
+          <Route path="/Offers" element={<Offers />} />
+        </Routes>
+      </Router>
     </>
   );
 }
