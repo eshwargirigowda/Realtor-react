d372b94029baf16316da7d66241929a34478e7dd
{eshwargirigowda} <{eshwar.tg.girigowda@gmail.com}>
2025-04-16 14:14:37 +0530

    Header and Nivagation

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Profile" element={<Profile />} />
